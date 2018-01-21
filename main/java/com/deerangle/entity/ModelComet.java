2f3cdf62a8b06bed914dbd43446ac1e067b3ec63
deerangle2 <ian.rehwinkel@kabelmail.de>
2018-01-21 19:53:20 +0100

    k

---
+++ main/java/com/deerangle/entity/ModelComet.java
@@ -0,0 +1,13 @@
+package com.deerangle.entity;
+
+import net.minecraft.client.model.ModelBase;
+import net.minecraft.entity.Entity;
+
+public class ModelComet extends ModelBase {
+
+	@Override
+	public void render(Entity entityIn, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch, float scale) {
+		super.render(entityIn, limbSwing, limbSwingAmount, ageInTicks, netHeadYaw, headPitch, scale);
+	}
+	
+}
--- main/java/com/deerangle/entity/RenderComet.java
+++ main/java/com/deerangle/entity/RenderComet.java
@@ -2,6 +2,7 @@ package com.deerangle.entity;
 
 import com.deerangle.main.InterstellarComets;
 
+import net.minecraft.client.model.ModelBase;
 import net.minecraft.client.renderer.culling.ICamera;
 import net.minecraft.client.renderer.entity.Render;
 import net.minecraft.client.renderer.entity.RenderBiped;
@@ -11,6 +12,8 @@ import net.minecraft.util.ResourceLocation;
 
 public class RenderComet extends Render<EntityComet> {
 
+	private ModelComet model;
+	
 	public RenderComet(RenderManager renderManager) {
 		super(renderManager);
 	}
@@ -18,6 +21,7 @@ public class RenderComet extends Render<EntityComet> {
 	@Override
 	public void doRender(EntityComet entity, double x, double y, double z, float entityYaw, float partialTicks) {
 		super.doRender(entity, x, y, z, entityYaw, partialTicks);
+		this.model.render(entity, 0, 0, 0, entityYaw, 0, 1.0F);
 	}
 	
 	@Override
